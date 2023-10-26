aab4bea62ca7c1198ebd15284dd471c5210a2d2a
Marcelo Segura <lordchelo@gmail.com>
2023-10-26 13:38:57 -0300

    v1.0.2

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,19 @@ const express = require('express')
 const router = express.Router()
 
 const Task = require('../models/task')
+const task = require('../models/task')
 
-router.get('/', (req, res) => {
-  res.render('index')
+router.get('/', async (req, res) => {
+  const tasks = await Task.find()
+  res.render('index', {
+    tasks
+  })
 })
 
 router.post('/add', async (req, res) => {
   const task = new Task(req.body)
   await task.save()
-  res.send('received')
+  res.redirect('/')
 })
 
 module.exports = router
