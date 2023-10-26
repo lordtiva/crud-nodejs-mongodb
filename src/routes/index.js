295492c7061e587451591d0d3ba81e5bd095896b
Marcelo Segura <lordchelo@gmail.com>
2023-10-26 18:00:16 -0300

    v1.0.3

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 const Task = require('../models/task')
-const task = require('../models/task')
 
 router.get('/', async (req, res) => {
   const tasks = await Task.find()
@@ -15,6 +14,30 @@ router.post('/add', async (req, res) => {
   const task = new Task(req.body)
   await task.save()
   res.redirect('/')
+})
+
+router.get('/done/:id', async (req, res) => {
+  const {id} = req.params
+  try {
+    const task = await Task.findById(id)
+    task.status = !task.status
+    await task.save()
+    res.redirect('/')
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Hubo un error al intentar actualizar la tarea')
+  }
+})
+
+router.get('/delete/:id', async (req, res) => {
+  const {id} = req.params
+  try {
+    await Task.deleteOne({_id: id})
+    res.redirect('/')
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Hubo un error al intentar eliminar la tarea')
+  }
 })
 
 module.exports = router
